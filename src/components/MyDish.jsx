75e9df0aee10555bef650b77f7c82c804ebe7c4e
Josh Howard <JoshHoward.Dev@gmail.com>
2022-12-21 22:13:23 -0500

    Add styling to ingredient container.

--- src/components/MyDish.jsx
+++ src/components/MyDish.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-alert */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
@@ -20,36 +22,56 @@ function MyDish(props) {
   });
 
   async function useAddDish(ingredientsArr) {
+    const name = document.querySelector('#dishNameInput').value;
+    const units = document.querySelector('#unitNameInput').value;
+
+    if (!name.length || !units.length || !ingredientsArr.length) {
+      return alert('You must enter a name, a unit name, and at least one ingredient.');
+    }
+
     const dbResponse = await fetch('/api/addDish', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         userId: 42,
-        name: document.querySelector('#dishNameInput').value,
-        units: document.querySelector('#unitNameInput').value,
+        name,
+        units,
         ingredientsArr,
       }),
     });
     const parsedResponse = await dbResponse.json();
-    navigate('/nutrientInfo', { state: { dbTable: 'dishes', dbId: parsedResponse.id } });
+    return navigate('/nutrientInfo', { state: { dbTable: 'dishes', dbId: parsedResponse.id } });
+  }
+
+  // Don't show ingredients section if there are no ingredients yet
+  const ingredientsDiv = [];
+  if (ingredientDivs.length) {
+    ingredientsDiv.push(
+      <div>
+        <div className="ingredientsContainer">
+          <h2>Ingredients</h2>
+          {ingredientDivs}
+        </div>
+        <button type="button" className="submitButton" onClick={() => { useAddDish(ingredients); }}>Add Dish</button>
+      </div>,
+    );
   }
 
   return (
     <div className="myDishContainer" key={ingredients}>
-      <h1>My Dish</h1>
-      <label htmlFor="unitNameInput">
-        Dish Name
-        <input type="text" name="dishNameInput" id="dishNameInput" />
-      </label>
-      <label htmlFor="unitNameInput">
-        Unit Name
-        <input type="text" name="unitNameInput" id="unitNameInput" />
-      </label>
-      <div className="ingredientsContainer">
-        <h2>Ingredients</h2>
-        {ingredientDivs}
+      <div className="myDishDetailsDiv">
+        <div className="dishDetailsInputs">
+          <div className="dishNameInputs">
+            <label htmlFor="dishNameInput">Dish Name</label>
+            <input type="text" name="dishNameInput" id="dishNameInput" placeholder="My Dish" />
+          </div>
+          <div className="unitNameInputs">
+            <label htmlFor="unitNameInput">Unit Name</label>
+            <input type="text" name="unitNameInput" id="unitNameInput" placeholder="Serving" />
+          </div>
+        </div>
+        {ingredientsDiv}
       </div>
-      <button type="button" onClick={() => { useAddDish(ingredients); }}>Add Dish</button>
     </div>
   );
 }
